d6c7c98093a7f1af4a548bcebac0fbefb13201b5
Serdeshnow <vserdes1977@mail.ru>
2025-04-03 19:36:02 +0300

    [init](project): commit

---
+++ frontend/src/app/providers/router.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from 'react-router-dom';
+import { HomePage } from '@/pages/home';
+// import { AboutPage } from '@/pages/about';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  // {
+  //   path: '/',
+  //   element: <LandingPage />,
+  // },
+  // {
+  //   path: '/about',
+  //   element: <AboutPage />,
+  // },
+]);
